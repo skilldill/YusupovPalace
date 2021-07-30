5ca1cacbf4f2c78f8ba35c6c666b0d9f2591c09a
Александр Утбанов <utbanovag@it2g.ru>
2021-07-31 00:08:16 +0300

    add appKey for auth request

--- back/controllers/room.py
+++ back/controllers/room.py
@@ -1,7 +1,14 @@
-from flask_restful import Resource
+from flask_restful import Resource, request, abort
 from utils import get_cursor, map_room
+from env import APP_KEY
+
 class Room(Resource):
     def get(self, room_id):
+        app_key = request.headers['appKey']
+
+        if app_key != APP_KEY:
+            return abort(403, message='Forbidden')
+
         query = '''
             SELECT room_id, name, description, audio, photo
             FROM rooms_data
--- back/controllers/rooms.py
+++ back/controllers/rooms.py
@@ -1,8 +1,13 @@
-from flask_restful import Resource
+from flask_restful import Resource, request, abort
 from utils import get_cursor, map_rooms
-
+from env import APP_KEY
 class RoomsList(Resource):
     def get(self):
+        app_key = request.headers['appKey']
+
+        if app_key != APP_KEY:
+            abort(403, message='Forbidden')
+
         query = '''
             SELECT * FROM rooms 
         '''
